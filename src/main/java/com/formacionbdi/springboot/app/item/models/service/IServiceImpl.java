20ff1129e541e18b233f0066d9434b7eb881f6f3
Mario95 <mario.masias95@gmail.com>
2024-05-23 13:38:51 -0500

    Se agrego Ribbon para balanceo de carga en RestTemplate sin Eureka

--- src/main/java/com/formacionbdi/springboot/app/item/models/service/IServiceImpl.java
+++ src/main/java/com/formacionbdi/springboot/app/item/models/service/IServiceImpl.java
@@ -20,7 +20,7 @@ public class IServiceImpl implements ItemService {
 	private RestTemplate clienteRest;
 	@Override
 	public List<Item> findAll() {
-		List<Producto> productos = Arrays.asList(clienteRest.getForObject("http://localhost:8001/listar", Producto[].class));
+		List<Producto> productos = Arrays.asList(clienteRest.getForObject("http://servicio-productos/listar", Producto[].class));
 		return productos.stream().map(p-> new Item(p, 1)).collect(Collectors.toList());
 	}
 
@@ -28,7 +28,7 @@ public class IServiceImpl implements ItemService {
 	public Item findById(Long id, Integer cantidad) {
 		Map<String, String> pathVariables = new HashMap<String, String>();
 		pathVariables.put("id", id.toString());
-		Producto producto = clienteRest.getForObject("http://localhost:8001/ver/{id}", Producto.class, pathVariables);
+		Producto producto = clienteRest.getForObject("http://servicio-productos/ver/{id}", Producto.class, pathVariables);
 		
 		return new Item(producto, cantidad);
 	}
